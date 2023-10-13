55d55115a6addafd656063562516850e1b9aeddd
Pikuto The Emperor <91076943+PIKUT0@users.noreply.github.com>
2023-10-13 17:02:00 +0300

    13.10.23
    C first, then gonna C++

--- C++.cpp
+++ C++.cpp
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <curses.h>
+#include <iostream>
+//Static then register goes :D
+int b = 0;
 
+/*int count (int i)
+{
+  static int c = 0;
+
+  if(i) return c;
+  else c++;
+  return 0;
+}*/
+int series(void)
+{
+  static int series_num = 0;
+  if(series_num == 255){return(series_num);}
+  else{series_num++;}
+}
+int main(void)
+{
+  A: do {
+   series();
+   b ++;
+  }
+  while(b != 255);
+  printf("count called %d times", series);
+  return 0;
+}
